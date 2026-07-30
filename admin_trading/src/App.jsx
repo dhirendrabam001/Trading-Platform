c1548a99be6baeb5a35cafd416d8a95517abebc1
dhirendra001 <dhirendrabam12345@gmail.com>
2026-07-30 12:56:53 +0530

    admin_fix_again

--- admin_trading/src/App.jsx
+++ admin_trading/src/App.jsx
@@ -4,13 +4,11 @@ import "./App.css";
 
 import Dashboard from "./layout/Dashboard";
 import { Route, Routes } from "react-router-dom";
-import Login from "./auth/Login/Login";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />}></Route>
-      <Route path="/login" element={<Login />}></Route>
     </Routes>
   );
 }
