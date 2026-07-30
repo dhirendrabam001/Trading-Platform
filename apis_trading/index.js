c7cdc1f53a569f5c2faf3b2e21e7e53e7a41040a
dhirendra001 <dhirendrabam12345@gmail.com>
2026-07-30 15:00:18 +0530

    updated latest code

--- apis_trading/index.js
+++ apis_trading/index.js
@@ -9,22 +9,25 @@ const connectDB = require("./config/connection");
 const app = express();
 
 // USE CORS
-const allowedOrigins = process.env.CLIENT_URL.split(",");
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true); // Postman support
-
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      } else {
-        return callback(new Error("CORS not allowed"));
-      }
-    },
-    credentials: true,
-  }),
+const allowedOrigins = process.env.CLIENT_URL.split(",").map((origin) =>
+  origin.trim(),
 );
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+
+    console.log("Blocked Origin:", origin);
+    return callback(new Error("CORS not allowed"));
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
